47ee4094315aac115f5e24bdb1937c6466b831d3
osamu shimada <osamu@quwans.jp>
2015-11-05 19:36:54 +0900

    第一次コンプリートコード

---
+++ takepic/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace takepic {
+	partial class Form1 {
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent() {
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.label_name = new System.Windows.Forms.Label();
+			this.button_toggleliveview = new System.Windows.Forms.Button();
+			this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.chkBxFileSave = new System.Windows.Forms.CheckBox();
+			this.chkBxFtpUpLoad = new System.Windows.Forms.CheckBox();
+			this.button_capture = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.rdBtnIIS = new System.Windows.Forms.RadioButton();
+			this.rdBtnAzure = new System.Windows.Forms.RadioButton();
+			this.txtBxURL = new System.Windows.Forms.TextBox();
+			this.btnCallSeverHub = new System.Windows.Forms.Button();
+			this.picBoxPhotImage = new System.Windows.Forms.PictureBox();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.tableLayoutPanel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.tableLayoutPanel3.SuspendLayout();
+			this.panel1.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.picBoxPhotImage)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 23.07692F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 76.92308F));
+			this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.picBoxPhotImage, 1, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(1017, 747);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+			this.tableLayoutPanel2.ColumnCount = 1;
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel2.Controls.Add(this.pictureBox2, 0, 3);
+			this.tableLayoutPanel2.Controls.Add(this.label_name, 0, 0);
+			this.tableLayoutPanel2.Controls.Add(this.button_toggleliveview, 0, 2);
+			this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 1);
+			this.tableLayoutPanel2.Controls.Add(this.btnCallSeverHub, 0, 4);
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 6;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 116F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 53F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 104F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 18F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(228, 691);
+			this.tableLayoutPanel2.TabIndex = 0;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox2.Location = new System.Drawing.Point(4, 206);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(220, 98);
+			this.pictureBox2.TabIndex = 0;
+			this.pictureBox2.TabStop = false;
+			// 
+			// label_name
+			// 
+			this.label_name.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.label_name.AutoSize = true;
+			this.label_name.Location = new System.Drawing.Point(83, 10);
+			this.label_name.Name = "label_name";
+			this.label_name.Size = new System.Drawing.Size(62, 12);
+			this.label_name.TabIndex = 1;
+			this.label_name.Text = "No Camera";
+			// 
+			// button_toggleliveview
+			// 
+			this.button_toggleliveview.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button_toggleliveview.Location = new System.Drawing.Point(4, 152);
+			this.button_toggleliveview.Name = "button_toggleliveview";
+			this.button_toggleliveview.Size = new System.Drawing.Size(220, 47);
+			this.button_toggleliveview.TabIndex = 3;
+			this.button_toggleliveview.Text = "Toggle Live View";
+			this.button_toggleliveview.UseVisualStyleBackColor = true;
+			this.button_toggleliveview.Click += new System.EventHandler(this.button_toggleliveview_Click);
+			// 
+			// tableLayoutPanel3
+			// 
+			this.tableLayoutPanel3.ColumnCount = 2;
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 114F));
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel3.Controls.Add(this.panel1, 0, 1);
+			this.tableLayoutPanel3.Controls.Add(this.button_capture, 1, 1);
+			this.tableLayoutPanel3.Controls.Add(this.groupBox1, 0, 0);
+			this.tableLayoutPanel3.Controls.Add(this.txtBxURL, 1, 0);
+			this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel3.Location = new System.Drawing.Point(4, 35);
+			this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+			this.tableLayoutPanel3.RowCount = 2;
+			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 47F));
+			this.tableLayoutPanel3.Size = new System.Drawing.Size(220, 110);
+			this.tableLayoutPanel3.TabIndex = 4;
+			// 
+			// panel1
+			// 
+			this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.panel1.Controls.Add(this.chkBxFileSave);
+			this.panel1.Controls.Add(this.chkBxFtpUpLoad);
+			this.panel1.Location = new System.Drawing.Point(3, 66);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(108, 41);
+			this.panel1.TabIndex = 3;
+			// 
+			// chkBxFileSave
+			// 
+			this.chkBxFileSave.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.chkBxFileSave.AutoSize = true;
+			this.chkBxFileSave.Checked = true;
+			this.chkBxFileSave.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.chkBxFileSave.Location = new System.Drawing.Point(4, 22);
+			this.chkBxFileSave.Name = "chkBxFileSave";
+			this.chkBxFileSave.Size = new System.Drawing.Size(82, 16);
+			this.chkBxFileSave.TabIndex = 4;
+			this.chkBxFileSave.Text = "ファイル保存";
+			this.chkBxFileSave.UseVisualStyleBackColor = true;
+			// 
+			// chkBxFtpUpLoad
+			// 
+			this.chkBxFtpUpLoad.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.chkBxFtpUpLoad.AutoSize = true;
+			this.chkBxFtpUpLoad.Location = new System.Drawing.Point(4, 3);
+			this.chkBxFtpUpLoad.Name = "chkBxFtpUpLoad";
+			this.chkBxFtpUpLoad.Size = new System.Drawing.Size(77, 16);
+			this.chkBxFtpUpLoad.TabIndex = 3;
+			this.chkBxFtpUpLoad.Text = "アップロード";
+			this.chkBxFtpUpLoad.UseVisualStyleBackColor = true;
+			// 
+			// button_capture
+			// 
+			this.button_capture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.button_capture.Location = new System.Drawing.Point(115, 64);
+			this.button_capture.Margin = new System.Windows.Forms.Padding(1);
+			this.button_capture.Name = "button_capture";
+			this.button_capture.Size = new System.Drawing.Size(104, 45);
+			this.button_capture.TabIndex = 2;
+			this.button_capture.Text = "写真を撮る";
+			this.button_capture.UseVisualStyleBackColor = true;
+			this.button_capture.Click += new System.EventHandler(this.button_capture_Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.rdBtnIIS);
+			this.groupBox1.Controls.Add(this.rdBtnAzure);
+			this.groupBox1.Location = new System.Drawing.Point(3, 3);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(108, 57);
+			this.groupBox1.TabIndex = 4;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "groupBox1";
+			// 
+			// rdBtnIIS
+			// 
+			this.rdBtnIIS.AutoSize = true;
+			this.rdBtnIIS.Checked = true;
+			this.rdBtnIIS.Location = new System.Drawing.Point(6, 35);
+			this.rdBtnIIS.Name = "rdBtnIIS";
+			this.rdBtnIIS.Size = new System.Drawing.Size(100, 16);
+			this.rdBtnIIS.TabIndex = 1;
+			this.rdBtnIIS.TabStop = true;
+			this.rdBtnIIS.Text = "Local IIS Sever";
+			this.rdBtnIIS.UseVisualStyleBackColor = true;
+			// 
+			// rdBtnAzure
+			// 
+			this.rdBtnAzure.AutoSize = true;
+			this.rdBtnAzure.Location = new System.Drawing.Point(6, 14);
+			this.rdBtnAzure.Name = "rdBtnAzure";
+			this.rdBtnAzure.Size = new System.Drawing.Size(101, 16);
+			this.rdBtnAzure.TabIndex = 0;
+			this.rdBtnAzure.Text = "Azure Web Site";
+			this.rdBtnAzure.UseVisualStyleBackColor = true;
+			this.rdBtnAzure.CheckedChanged += new System.EventHandler(this.rdBtnAzure_CheckedChanged);
+			// 
+			// txtBxURL
+			// 
+			this.txtBxURL.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.txtBxURL.Location = new System.Drawing.Point(117, 12);
+			this.txtBxURL.Multiline = true;
+			this.txtBxURL.Name = "txtBxURL";
+			this.txtBxURL.Size = new System.Drawing.Size(100, 38);
+			this.txtBxURL.TabIndex = 5;
+			// 
+			// btnCallSeverHub
+			// 
+			this.btnCallSeverHub.Location = new System.Drawing.Point(4, 311);
+			this.btnCallSeverHub.Name = "btnCallSeverHub";
+			this.btnCallSeverHub.Size = new System.Drawing.Size(196, 23);
+			this.btnCallSeverHub.TabIndex = 5;
+			this.btnCallSeverHub.Text = "デバッグ用：Nothig";
+			this.btnCallSeverHub.UseVisualStyleBackColor = true;
+			this.btnCallSeverHub.Click += new System.EventHandler(this.btnCallSeverHub_Click);
+			// 
+			// picBoxPhotImage
+			// 
+			this.picBoxPhotImage.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.picBoxPhotImage.Location = new System.Drawing.Point(237, 3);
+			this.picBoxPhotImage.Name = "picBoxPhotImage";
+			this.picBoxPhotImage.Size = new System.Drawing.Size(777, 741);
+			this.picBoxPhotImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.picBoxPhotImage.TabIndex = 1;
+			this.picBoxPhotImage.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1017, 747);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel2.ResumeLayout(false);
+			this.tableLayoutPanel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.tableLayoutPanel3.ResumeLayout(false);
+			this.tableLayoutPanel3.PerformLayout();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.picBoxPhotImage)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Label label_name;
+		private System.Windows.Forms.Button button_capture;
+		private System.Windows.Forms.Button button_toggleliveview;
+		private System.Windows.Forms.PictureBox picBoxPhotImage;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+		private System.Windows.Forms.CheckBox chkBxFtpUpLoad;
+		private System.Windows.Forms.Button btnCallSeverHub;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.CheckBox chkBxFileSave;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.RadioButton rdBtnIIS;
+		private System.Windows.Forms.RadioButton rdBtnAzure;
+		private System.Windows.Forms.TextBox txtBxURL;
+	}
+}
+
---
+++ takepic/Form1.cs
@@ -0,0 +1,424 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;//FTP Up Load用
+using System.Text.RegularExpressions;//ファイル名抽出用
+using Nikon;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Client;
+
+using System.Diagnostics;
+
+
+
+namespace takepic {
+	public partial class Form1 : Form {
+		private NikonManager _Manager;
+		private NikonDevice _Device;
+		private System.Windows.Forms.Timer _LiveViewTimer;
+		private Image _PhotImg;
+		private Image _LiveViewImg;
+		private string _LiveViewImgFlNm = "liveViewImg.jpg";
+
+		private HubConnection _HubConnection;
+		private IHubProxy _HubProxy;
+
+
+		public Form1() {
+			InitializeComponent();
+
+			// Disable buttons
+			ToggleButtons(false);
+
+			// ライブビューのフレームレートをタイマーでセット
+			_LiveViewTimer = new System.Windows.Forms.Timer();
+			_LiveViewTimer.Tick += new System.EventHandler(liveViewTimer_Tick);
+			_LiveViewTimer.Interval = 1000;//１秒間に１回　1000/30で 30フレーム／秒
+
+			// Nikon managerオブジェクト作成しイベントハンドラを登録する。
+			_Manager = new NikonManager("Type0011.md3");
+			_Manager.DeviceAdded += new DeviceAddedDelegate(manager_DeviceAdded);
+			_Manager.DeviceRemoved += new DeviceRemovedDelegate(manager_DeviceRemoved);
+
+
+			// SignalR オブジェクト作成しHubのイベントハンドラを登録する。
+			setHubConnection();
+
+		}
+
+		//（注）グローバル変数【_HubConnection】を編集しています。イベントハンドラ中で実行するので、大域変数を利用しなければならなかったため。
+		void setHubConnection() {
+			//現在稼働中のハブコネクションがあれば、いったん廃棄
+			if (_HubConnection != null) {
+				_HubConnection.Stop();
+				_HubConnection.Dispose();
+			}
+			if (rdBtnAzure.Checked)
+				_HubConnection = new HubConnection("http://picupsignalr.azurewebsites.net/");
+			else
+				_HubConnection = new HubConnection("http://LocalHost/");
+
+			txtBxURL.Text = _HubConnection.Url.ToString();
+
+			// HubConnectionをStartする前にHelloHubクラスに対するproxyを作っておく
+			_HubProxy = _HubConnection.CreateHubProxy("WatchFolder");
+
+			// サーバーから【PushShutter】という通知が送られてきたら【takeAPicture】を実行するデリゲートをのメソッドを登録
+			_HubProxy.On<string>("PushShutter", takeAPicture);
+
+			_HubConnection.Start();
+		}
+
+
+		//シャッターボタン押下指令　拝命 
+		void takeAPicture(string s) {
+			// 説明文
+			#region
+			//↓は、コントロールが作成されたスレッドとは別スレッドから呼び出されるため、エラーとなる；
+			//ToggleButtons(false);
+			//よって、Invokeとデリゲートを使用(Invokeは、元のスレッドと同じスレッドハンドルでデリゲートを実行する)
+			//------------------------------------------------------------------------------------------------------
+			// stringを引数に取りintを返すsetFocusメソッドを参照する'DlgtFnc'デリゲートを生成
+			//		Func<string, int> DlgtFnc = setFocus;
+			//	↑を、こんな風に↓インラインで処理を記述したい。
+			//		Func<string, int> DlgtFnc = public int setFocus(String str) { ToggleButtons(false);return 0;}
+			// そこで匿名メソッドを使用して、【setFocus】メソッド名を省略
+			//		Func<string, int> DlgtFnc = delegate (string str) { ToggleButtons(false);return 0;}
+			// 更にラムダ式
+			//		Func<string, int> DlgtFnc = (string str) => { ToggleButtons(false);return 0;};
+			// 更に更に省略
+			//		Func<string, int> DlgtFnc = str => { ToggleButtons(false);return 0;};
+			// これをInvokeで利用するため、Func型宣言と実行を同時に行うため匿名メソッドを使用
+			//		Invoke((MethodInvoker)delegate () { ToggleButtons(false);});
+			// 更に、ラムダ式を利用
+			//		Invoke((MethodInvoker)(() => { ToggleButtons(false); }));
+			//-------------------------------------------------------------------------------------------------------
+			#endregion
+
+			Task.Factory.StartNew(() => {//すぐにスタート
+				Invoke((MethodInvoker)(() => {
+					if (_Device == null) {
+						return;
+					}
+
+					ToggleButtons(false);
+
+					try {
+						_Device.Capture();
+					} catch (NikonException ex) {
+						Console.WriteLine("シャッター押下に失敗しました。" + ex.Message);
+						ToggleButtons(true);
+					}
+					picBoxPhotImage.Image = null;
+				}));
+			});
+		}
+
+
+		//ローカルIIS接続時のファイル書き込み終了イベント：写真画像及びライブ保存完了時に呼び出される。
+		//private void writeImgFileCallback( IAsyncResult ar) {
+		private void writeImgFileCallback(IAsyncResult ar) {
+			//保存したファイル名を取得するには、↓ではだめ
+			//private void writePhotFileCallback(string fileName) {
+			//ファイル名を取り出すため、FileStreamから取り出すのはダサい
+			//System.IO.FileStream fs = (System.IO.FileStream)ar.AsyncState;
+			//よって、flStream.BeginWriteのイベントハンドラのstate パラメータを使用する（因みにFrameWork4.5からはWriteAsync()を使用
+			//使い方は、【Socket.BeginConnect() のヘルプの「解説」】より
+			//【state パラメータを使用して、 Socket を BeginConnect に渡さなければなりません。他の情報がコールバックに必要な場合は、小さなクラスを作成して Socket などの必要な情報を保持します。
+			//このクラスのインスタンスは、 state パラメータを使用して BeginConnect メソッドに渡します。】cf:http://dobon.net/vb/bbs/log3-20/11887.html
+			//よって、BeginWrite(byte[] buffer,int offset,int size,	AsyncCallback callback,	Object state)
+			//の、stateオブジェクトに渡す自前クラス設定する。
+
+			EvHndParamClass param = (EvHndParamClass)ar.AsyncState;
+			//保存した写真画像名を抽出
+			//Regex reg = new Regex(@"phot[0-9].*[.]jpg$");
+			//Match mtchStr = reg.Match(fs.Name);
+
+			//サーバの「PhotChangeハブ」へ保存ファイル名を通知
+			try {
+				//_HubProxy.Invoke("PhotChange", mtchStr.ToString()).Wait();
+				_HubProxy.Invoke("PhotChange", param.str).Wait();
+			} catch (Exception ex) {
+				Console.WriteLine("err:" + ex.Message + "Webサーバ呼び出しエラーです。");
+			}
+		}
+
+		//ライブ画像の書き込み終了時に呼び出されるメソッド：ここから【PhotChange】Hubを呼び出す。
+		private void writeLiveImgFileCallback(IAsyncResult ar) {
+			//			Console.WriteLine("LovView映像配信中");
+			//サーバの【PhotChange】Hubへ保存したファイル名を通知
+			try {
+				_HubProxy.Invoke("PhotChange", _LiveViewImgFlNm).Wait();
+			} catch (Exception ex) {
+				Console.WriteLine("err:" + ex.Message + "Webサーバ呼び出しエラーです。");
+			}
+		}
+
+
+		// 撮影画像の準備完了時のイベントメソッド：撮影画像を保存し、保存完了時に【writeImgFileCallback】Hubを呼び出す。
+		void device_ImageReady(NikonDevice sender, NikonImage image) {
+			using (MemoryStream memStream = new MemoryStream(image.Buffer)) {
+				// 画像データの検証オン
+				_PhotImg = System.Drawing.Image.FromStream(memStream);
+
+				//保存する写真画像のユーニークなファイル名
+				//確認用に、フォームに撮った写真をフォームに表示
+				picBoxPhotImage.Image = _PhotImg;
+
+				//写真画像保存時のファイル名にユニークな名前を付ける。
+				string fileUnqNm = "phot" + DateTime.Now.Month.ToString() + "_" + DateTime.Now.Day.ToString() + "_" + DateTime.Now.Hour.ToString() + "_" + DateTime.Now.Minute.ToString() + "_" + DateTime.Now.Second.ToString() + "_" + DateTime.Now.Millisecond.ToString() + ".jpg";
+
+				#region 条件分岐：ローカルサーバへのファイル保存がONなら
+				if (chkBxFileSave.Checked == true) {
+					try {//ファイル保存
+						Console.WriteLine("写真画像保存開始");
+
+						//flStream.BeginWriteイベントハンド呼び出し時に使用するステイタスパラメータクラスのインスタンスを作成
+						using (EvHndParamClass param = new EvHndParamClass()) {
+
+							// ファイル書き込み終了時のイベントハンドラで実行されるコールバックメソッド内でファイル名を利用すため
+							//そのイベントハンドラに渡すパラメータの、stプロパティにファイル名をセット						
+							param.str = fileUnqNm;
+
+							//非同期書き込み用のファイルストリームを作成
+							using (FileStream flStream = new FileStream(@"C:\inetpub\wwwroot\images/" + fileUnqNm, FileMode.Create,
+																		FileAccess.Write, FileShare.ReadWrite, image.Buffer.Length, true)) {
+								//非同期書き込み、書き込み終了時に呼び出されるメソッド【writeFileCallback】
+								//flStream.BeginWrite(image.Buffer, 0, image.Buffer.Length, new AsyncCallback(writeImgFileCallback), flStream);
+								flStream.BeginWrite(image.Buffer, 0, image.Buffer.Length, new AsyncCallback(writeImgFileCallback), param);
+								//							await writer.WriteAsync(input);
+							}
+						}
+					} catch (Exception ex) {
+						Console.WriteLine("err:" + ex.Message + " 写真画像ファイル書き込み失敗!");
+					}
+				}
+				#endregion
+
+				#region 条件分岐: FTPアップロードチェックボックスがONなら
+				if (chkBxFtpUpLoad.Checked == true) {
+					try {//↓は、FTPにて写真アップロードの【場合】です。
+						WebClient wc = new WebClient();
+						//アップロード完了時イベントを登録
+						wc.UploadDataCompleted += (s, e) => {
+							Console.WriteLine("アップロード完了");
+							_HubProxy.Invoke("PhotChange", fileUnqNm).Wait();
+						};
+
+						Uri u = new Uri("" + fileUnqNm);
+						//認証設定
+						wc.Credentials = new NetworkCredential(@"", "");
+
+						Console.WriteLine("写真画像FTPアップロード開始");
+						wc.UploadDataAsync(u, image.Buffer);
+					} catch (Exception ex) {
+						//MessageBox.Show(ex.ToString());
+						Console.WriteLine("err:" + ex.Message + " 写真画像FTPアップロード失敗!");
+					}
+				}
+				#endregion
+			}
+		}
+
+		// タイマー割り込みでライブビューを表示するイベントメソッド(画像ファイルのローカル保存 Or ＦＴＰ)
+		void liveViewTimer_Tick(object sender, EventArgs e) {
+			// Get live view image
+			NikonLiveViewImage image = null;
+
+			try {
+				image = _Device.GetLiveViewImage();
+			} catch (NikonException) {
+				_LiveViewTimer.Stop();
+			}
+
+			if (image != null) {
+				MemoryStream memStream = new MemoryStream(image.JpegBuffer);
+				//確認用に、フォームに撮った写真をフォームに表示
+				_LiveViewImg = System.Drawing.Image.FromStream(memStream);
+				picBoxPhotImage.Image = _LiveViewImg;
+
+				#region 条件分岐：ローカルサーバへのファイル保存がONなら
+				if (chkBxFileSave.Checked == true) {
+					try {//ファイルを上書きで保存（ファイル名固定）
+
+						Console.WriteLine("ライブ画像保存開始");
+
+						//非同期書き込み用のファイルストリームを作成
+						using (FileStream flStream = new FileStream(@"C:\inetpub\wwwroot\images/" + _LiveViewImgFlNm, FileMode.Create,
+																	FileAccess.Write, FileShare.ReadWrite, image.JpegBuffer.Length, true)) {
+
+							//flStream.BeginWriteイベントハンド呼び出し時に使用するステイタスパラメータクラスのインスタンスを作成
+							using (EvHndParamClass param = new EvHndParamClass()) {
+								//ファイル書き込み終了時のイベントハンドラで実行されるコールバックメソッド内でファイル名を利用すため
+								//そのイベントハンドラに渡すパラメータの、stプロパティにファイル名をセット
+								param.str = _LiveViewImgFlNm;
+
+								//非同期に書き込み処理を実行する。当該イベント完了時に呼び出されるメソッド【writeImgFileCallback】を設定
+								flStream.BeginWrite(image.JpegBuffer, 0, image.JpegBuffer.Length, new AsyncCallback(writeImgFileCallback), param);
+								//	await writer.WriteAsync(input);//←Frame Work 4.5以上でないと使えない？
+							}
+						}
+					} catch (Exception ex) {
+						Console.WriteLine("err:" + ex.Message + " ライブ画像ファイル書き込み失敗!");
+					}
+				}
+				#endregion
+
+				#region 条件分岐: FTPアップロードチェックボックスがONなら
+				if (chkBxFtpUpLoad.Checked == true) {
+					try {//↓は、FTPにて写真アップロードの【場合】です。
+						WebClient wc = new WebClient();
+						//アップロード完了時イベントを登録
+						wc.UploadDataCompleted += (s, ev) => {
+							Console.WriteLine("アップロード完了");
+							_HubProxy.Invoke("PhotChange", _LiveViewImgFlNm).Wait();
+						};
+
+						Uri u = new Uri("" + _LiveViewImgFlNm);
+						//認証設定
+						wc.Credentials = new NetworkCredential();
+
+						Console.WriteLine("写真画像FTPアップロード開始");
+						wc.UploadDataAsync(u, image.JpegBuffer);
+					} catch (Exception ex) {
+						//MessageBox.Show(ex.ToString());
+						Console.WriteLine("err:" + ex.Message + " 写真画像FTPアップロード失敗!");
+					}
+				}
+				#endregion
+
+			}
+		}
+
+		// 写真撮影ボタンクリック時のイベントメソッド(コールバック)
+		private void button_capture_Click(object sender, EventArgs e) {
+			if (_Device == null) {
+				return;
+			}
+
+			ToggleButtons(false);
+
+			try {
+				// シャッターを押し、画像をバッファへ転送する。
+				//画像が準備できれば【device_ImageR】イベント発火
+				_Device.Capture();
+			} catch (NikonException ex) {
+				Console.WriteLine("err:" + ex.Message + " 写真画像FTPアップロード失敗!");
+				ToggleButtons(true);
+			}
+
+			picBoxPhotImage.Image = null;
+		}
+
+		//デバッグ用：何もしない。
+		private void btnCallSeverHub_Click(object sender, EventArgs e) {
+
+			MessageBox.Show("何もしないことにしました。");
+			//Hub呼び出しテスト　_HubProxy.Invoke("PhotChange", "default.jpg").Wait();
+
+		}
+
+		// 撮影完了時にトグルボタンをONにするイベントメソッド(コールバック)
+		void device_CaptureComplete(NikonDevice sender, int data) {
+			// Re-enable buttons when the capture completes
+			ToggleButtons(true);
+		}
+
+		// ライブビュー表示／非表示トグルボタン押下時のイベントメソッド（コールバック）
+		void ToggleButtons(bool enabled) {
+			this.button_capture.Enabled = enabled;
+			this.button_toggleliveview.Enabled = enabled;
+		}
+
+		// ライブビュー開始／中止トグルボタン押下時のイベントメソッド(コールバック)
+		private void button_toggleliveview_Click(object sender, EventArgs e) {
+			if (_Device == null) {
+				return;
+			}
+			if (_Device.LiveViewEnabled) {
+				_Device.LiveViewEnabled = false;
+				_LiveViewTimer.Stop();
+				picBoxPhotImage.Image = null;
+			} else {
+				_Device.LiveViewEnabled = true;
+				_LiveViewTimer.Start();
+			}
+		}
+
+		// アプリ終了時のフォームクローズイベントメソッド(コールバック)
+		protected override void OnClosing(CancelEventArgs e) {
+			// Disable live view (in case it's enabled)
+			if (_Device != null) {
+				_Device.LiveViewEnabled = false;
+			}
+
+			// Shut down the Nikon manager
+			_Manager.Shutdown();
+			base.OnClosing(e);
+		}
+
+		// カメラ接続開始時のイベントメソッド(コールバック)
+		void manager_DeviceAdded(NikonManager sender, NikonDevice device) {
+			this._Device = device;
+
+			// Set the device name
+			label_name.Text = device.Name;
+
+			// Enable buttons
+			ToggleButtons(true);
+
+			// 撮影画像のバッファへの転送完了イベント
+			device.ImageReady += new ImageReadyDelegate(device_ImageReady);
+			// 撮影完了時のイベント(トグルボタンOFF→ON）
+			device.CaptureComplete += new CaptureCompleteDelegate(device_CaptureComplete);
+		}
+
+		// カメラ接続終了時のイベントメソッド(コールバック)
+		void manager_DeviceRemoved(NikonManager sender, NikonDevice device) {
+			this._Device = null;
+
+			// Stop live view timer
+			_LiveViewTimer.Stop();
+
+			// Clear device name
+			label_name.Text = "No Camera";
+
+			// Disable buttons
+			ToggleButtons(false);
+
+			// Clear live view picture
+			picBoxPhotImage.Image = null;
+		}
+
+		//Webサーバー切り替え時に、Hubコネクション再構築を行う
+		private void rdBtnAzure_CheckedChanged(object sender, EventArgs e) {
+			setHubConnection();
+		}
+	}
+
+	//flStream.BeginWriteイベントハンド呼び出し時に使用するステイタスパラメータクラスの宣言
+	public class EvHndParamClass : IDisposable {
+		public string str;
+
+		//コンストラクタ
+		public EvHndParamClass() {
+			str = "default";
+		}
+		//デストラクタ
+		~EvHndParamClass() {
+			Console.WriteLine("EvHndParamClassのインスタンスのデストラクタが呼び出されました。");
+		}
+		//Using区で確実にディスポーズするため
+		void IDisposable.Dispose() {
+			Console.WriteLine("EvHndParamClassのインスタンスが、Using区の終了でディスポーズされました。");
+		}
+	}
+}
